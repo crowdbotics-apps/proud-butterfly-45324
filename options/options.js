7b656ea92556683e838e4aa27845cd26ce73d374
Crowdbotics <team@crowdbotics.com>
2024-01-08 21:48:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-butterfly-45324",
+  url: "https://proud-butterfly-45324.botics.co",
+  api: "https://proud-butterfly-45324.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
